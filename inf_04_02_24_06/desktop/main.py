81042c0f4b13f96743081421ef45649d90c9086d
Wojciech Choroś <wojtekchoros06@gmail.com>
2025-09-25 11:21:08 +0200

    feat: inf_04_02_24_06/console

--- inf_04_02_24_06/desktop/main.py
+++ inf_04_02_24_06/desktop/main.py
@@ -1,5 +1,5 @@
 from PyQt6.QtWidgets import (
-    QMainWindow, 
+    QMainWindow,
     QApplication,
     QLabel,
     QPushButton,
@@ -8,28 +8,38 @@ from PyQt6.QtGui import QPixmap, QIcon
 from PyQt6.QtCore import QSize
 import sys
 
+
 class Song:
-    def __init__(self):
-        self.author = ""
-        self.name = ""
-        self.number_of_tracks = ""
-        self.year = ""
-        self.downloads = 0
+    def __init__(
+        self,
+        author: str = None,
+        name: str = None,
+        number_of_tracks: int = None,
+        year: int = None,
+        downloads: int = None,
+    ):
+        self.author = author
+        self.name = name
+        self.number_of_tracks = number_of_tracks
+        self.year = year
+        self.downloads = downloads
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Moje dźwięki, Wykonał: 00000000000")
         self.setGeometry(50, 50, 1100, 300)
-        self.load_albums()
+        self.load_songs()
         self.initUI()
 
     def set_texts(self):
         self.author_label.setText(self.songs[self.curr_song].author)
         self.name_label.setText(self.songs[self.curr_song].name)
-        self.tracks_year_label.setText(f"{self.songs[self.curr_song].number_of_tracks} utworów         {self.songs[self.curr_song].year}")
+        self.tracks_year_label.setText(
+            f"{self.songs[self.curr_song].number_of_tracks} utworów         {self.songs[self.curr_song].year}"
+        )
         self.downloads_label.setText(f"{self.songs[self.curr_song].downloads}")
-
 
     def increment_number_of_downloads(self):
         self.songs[self.curr_song].downloads += 1
@@ -37,14 +47,14 @@ class MainWindow(QMainWindow):
 
     def switch_to_next_song(self):
         if len(self.songs) - 1 != self.curr_song:
-            self.curr_song += 1 
+            self.curr_song += 1
         else:
-            self.curr_song = 0 
+            self.curr_song = 0
         self.set_texts()
 
     def switch_to_previous_song(self):
         if self.curr_song != 0:
-            self.curr_song -= 1 
+            self.curr_song -= 1
         else:
             self.curr_song = len(self.songs) - 1
         self.set_texts()
@@ -83,18 +93,16 @@ QPushButton {
 }
 
 """)
-        
+
         vinyl_pixmap = QPixmap("data/obraz.png")
-        
+
         self.left_btn = QPushButton(self)
         self.left_btn.setIcon(QIcon("data/obraz3.png"))
         self.left_btn.setObjectName("left_btn")
         self.left_btn.setIconSize(QSize(102, 70))
         self.left_btn.setGeometry(25, self.height() // 2 - 35, 95, 70)
         self.left_btn.clicked.connect(self.switch_to_previous_song)
-        
 
-        
         self.right_btn = QPushButton(self)
         self.right_btn.setObjectName("right_btn")
         self.right_btn.setIcon(QIcon("data/obraz2.png"))
@@ -106,13 +114,12 @@ QPushButton {
         self.vinyl_img.setPixmap(vinyl_pixmap)
         self.vinyl_img.setGeometry(150, 25, 200, 200)
 
-
-
-
-
         self.author_label = QLabel(self.songs[self.curr_song].author, self)
         self.name_label = QLabel(self.songs[self.curr_song].name, self)
-        self.tracks_year_label = QLabel(f"{self.songs[self.curr_song].number_of_tracks} utworów         {self.songs[self.curr_song].year}", self)
+        self.tracks_year_label = QLabel(
+            f"{self.songs[self.curr_song].number_of_tracks} utworów         {self.songs[self.curr_song].year}",
+            self,
+        )
         self.downloads_label = QLabel(f"{self.songs[self.curr_song].downloads}", self)
 
         self.author_label.setGeometry(370, 0, 1000, 150)
@@ -129,13 +136,12 @@ QPushButton {
 
         self.download_btn.clicked.connect(self.increment_number_of_downloads)
 
-
-    def load_albums(self):
+    def load_songs(self):
         self.curr_song = 0
         self.songs = []
-        with open('data/Data.txt') as file:
+        with open("data/Data.txt") as file:
             line = file.readline().strip()
-            while(line):
+            while line:
                 song = Song()
                 song.author = line
                 song.name = file.readline().strip()
