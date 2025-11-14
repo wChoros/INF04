bebafa00918a8f8f06558a897bd65cc0fd3e913d
Wojciech Choroś <wojtekchoros06@gmail.com>
2025-11-14 18:15:57 +0100

    fraktal memory

--- additional_excerisises/desktop/memory/main.py
+++ additional_excerisises/desktop/memory/main.py
@@ -1,47 +1,52 @@
 from PyQt6.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QMessageBox
 import sys
 import random
-from PyQt6.QtCore import QTimer, Qt
+from PyQt6.QtCore import QTimer, Qt, QSize
+from PyQt6.QtGui import QIcon
 
 
 GAME_TIME = 30
 TILE_SIZE = 125
 
+
 class TileButton(QPushButton):
-    def __init__(self, parent, memory_card_color):
+    def __init__(self, parent, memory_card_image):
         super().__init__(parent)
         self.is_up = False
         self.setStyleSheet("background-color: #d8dee9")
-        self.memory_card_color = memory_card_color
+        self.memory_card_image = memory_card_image
+        self.setIcon(QIcon("./images/default.jpg"))
+        self.setIconSize(QSize(TILE_SIZE, TILE_SIZE))
 
     def turn_up(self):
         self.is_up = True
-        self.setStyleSheet(f"background-color: {self.memory_card_color}")
+        self.setIcon(QIcon(self.memory_card_image))
 
     def turn_down(self):
         self.is_up = False
         self.setStyleSheet("background-color: #d8dee9")
+        self.setIcon(QIcon("./images/default.jpg"))
 
 
-memory_colors = [
-    "#bf616a",
-    "#d08770",
-    "#ebcb8b",
-    "#a3be8c",
-    "#b48ead",
-    "#88c0d0",
+memory_images = [
+    "./images/cantors_set.jpg",
+    "./images/dragon_curve.jpg",
+    "./images/fractal_tree.jpg",
+    "./images/kochs_curve.jpg",
+    "./images/sierpinsis_triangle.jpg",
+    "./images/sierpinskis_carpet.jpg",
 ]
 
-memory_colors = memory_colors + memory_colors
-random.shuffle(memory_colors)
+memory_images = memory_images + memory_images
+random.shuffle(memory_images)
 
 
 class MainWindow(QMainWindow):
     def set_time_left(self):
-        self.timer_label.setText(f"Time left: {round(self.time_left,2)}s")
+        self.timer_label.setText(f"Time left: {round(self.time_left, 2)}s")
 
     def handle_timer_tick(self):
-        if self.is_timer_blocked:
+        if self.is_timer_blocked or not self.first_click_happened:
             return
 
         if self.time_left > 0:
@@ -53,21 +58,23 @@ class MainWindow(QMainWindow):
             self.reset_game()
 
     def reset_game(self):
+        self.status_label.setText("Mismatched 0 times")
+        self.timer_label.setText(f"Time left: {GAME_TIME}s")
+        self.first_click_happened = False
         self.curr_up_tile = None
         self.num_of_missmatches = 0
         self.tiles_blocked = False
         self.time_left = GAME_TIME
         self.is_timer_blocked = False
         self.game_timer.start(10)
-        self.status_label.setText("Mismatched 0 times")
         for tile in self.tiles:
             tile.turn_down()
 
-        random.shuffle(memory_colors)
+        random.shuffle(memory_images)
 
         for i in range(3):
             for j in range(4):
-                self.tiles[i * 4 + j].memory_card_color = memory_colors[i * 4 + j]
+                self.tiles[i * 4 + j].memory_card_image = memory_images[i * 4 + j]
 
     def is_game_won(self):
         for tile in self.tiles:
@@ -79,11 +86,11 @@ class MainWindow(QMainWindow):
         message = QMessageBox()
         message.setText(
             (
-                "Congrats, u won."
+                "Khhh, gratulacje, może policzysz teraz całki?"
                 if is_positive
-                else "Stupid looser, you cant win the chilish game xd."
+                else "Jak ty tego nie wygrałeś, przecież to jest trywialne?"
             )
-            + f"\nNum of missmatches: {self.num_of_missmatches}"
+            + f"\Ilość prób nietrafionych: {self.num_of_missmatches}"
         )
         message.exec()
 
@@ -96,14 +103,18 @@ class MainWindow(QMainWindow):
         QTimer.singleShot(time_period * 1000, do_flip)
 
     def handle_click(self, button_index):
+        if not self.first_click_happened:
+            self.first_click_happened = True
+            self.game_timer.start(10)
+
         if self.tiles[button_index].is_up or self.tiles_blocked:
             return
         self.tiles[button_index].turn_up()
 
         if self.curr_up_tile is not None:
             if (
-                self.tiles[button_index].memory_card_color
-                != self.tiles[self.curr_up_tile].memory_card_color
+                self.tiles[button_index].memory_card_image
+                != self.tiles[self.curr_up_tile].memory_card_image
             ):
                 self.tiles_blocked = True
                 self.num_of_missmatches += 1
@@ -143,6 +154,7 @@ QPushButton{
     color: #3b4252;
 }
 """)
+        self.first_click_happened = False
         self.is_timer_blocked = False
         self.time_left = GAME_TIME
         self.curr_up_tile = None
@@ -150,7 +162,7 @@ QPushButton{
         self.tiles_blocked = False
 
         self.restart_button = QPushButton("Restart", self)
-        self.restart_button.setGeometry(10, 590, 150,100)
+        self.restart_button.setGeometry(10, 590, 150, 100)
         self.restart_button.clicked.connect(self.reset_game)
 
         self.status_label = QLabel("Mismatched 0 times", self)
@@ -172,8 +184,13 @@ QPushButton{
 
         for i in range(3):
             for j in range(4):
-                self.tiles.append(TileButton(self, memory_colors[i * 4 + j]))
-                self.tiles[-1].setGeometry(90 + j * (TILE_SIZE+5), i * (TILE_SIZE+5) + 90, TILE_SIZE, TILE_SIZE)
+                self.tiles.append(TileButton(self, memory_images[i * 4 + j]))
+                self.tiles[-1].setGeometry(
+                    90 + j * (TILE_SIZE + 5),
+                    i * (TILE_SIZE + 5) + 90,
+                    TILE_SIZE,
+                    TILE_SIZE,
+                )
                 self.tiles[-1].clicked.connect(
                     lambda _, index=(i * 4 + j): self.handle_click(index)
                 )
